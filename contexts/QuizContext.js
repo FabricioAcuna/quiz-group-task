05a0e7c7c7f3242158f94d164788e5ab5e529bbd
FabricioAcuna <fabricio.acuna@chasacademy.se>
2025-02-06 16:21:09 +0100

    quiz test

--- contexts/QuizContext.js
+++ contexts/QuizContext.js
@@ -4,11 +4,11 @@ export const QuizContext = createContext();
 
 export default function QuizProvider({ children }) {
   const [questions, setQuestions] = useState([
-    { text: "Vad är...?", alt: ["a", "b", "c"], correct: 0 },
-    { text: "Vad är...?", alt: ["a", "b", "c"], correct: 0 },
-    { text: "Vad är...?", alt: ["a", "b", "c"], correct: 0 },
-    { text: "Vad är...?", alt: ["a", "b", "c"], correct: 0 },
-    { text: "Vad är...?", alt: ["a", "b", "c"], correct: 0 },
+    { text: "Vad är Sveriges huvudstad?", alt: ["a", "b", "c"], correct: 0 },
+    { text: "Vad är Norges huvudstad?", alt: ["a", "b", "c"], correct: 1 },
+    { text: "Vad är Tysklands huvudstad?", alt: ["a", "b", "c"], correct: 2 },
+    { text: "Vad är Finlands huvudstad?", alt: ["a", "b", "c"], correct: 0 },
+    { text: "Vad är Spaniens huvudstad?", alt: ["a", "b", "c"], correct: 2 },
   ]);
 
   return (
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,5 +1,21 @@
 import { QuizContext } from "@/contexts/QuizContext";
+import { useContext } from "react";
+
 export default function Quiz() {
   const { questions } = useContext(QuizContext);
-  return <div></div>;
+  return (
+    <div>
+      <div className="">
+        {questions.map((question, index) => {
+          return (
+            <div key={index}>
+              <p>{question.text}</p>
+              <button>{question.alt}</button>
+              <p>{question.score}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
