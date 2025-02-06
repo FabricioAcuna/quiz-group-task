e79187bcea2c1fcdc04c5e3902a07aea159e4c6b
Jenny Digné <jenny_digne@hotmail.com>
2025-02-06 16:30:16 +0100

    testat lägga till css

--- pages/quiz.js
+++ pages/quiz.js
@@ -5,28 +5,30 @@ export default function Quiz() {
   const { questions } = useContext(QuizContext);
   return (
     <div>
-      <div className="">
+      <div className="min-h-screen bg-blue-500 flex flex-col justify-center items-center">
+      <h1 className="text-white text-5xl font-bold text-center mb-6">Welcome to the quiz</h1>
         {questions.map((question, index) => {
           return (
             <div key={index}>
               <p>{question.text}</p>
-              <button>{question.alt}</button>
+              <button className="px-6 py-3 text-lg font-semibold text-white bg-blue-900 rounded-lg hover:bg-blue-950">{question.alt}</button>
               <p>{question.score}</p>
             </div>
           );
         })}
       </div>
     </div>
-  );
-
-  return (
-    <div className="min-h-screen bg-blue-500 flex flex-col justify-center items-center">
-      <h1 className="text-white text-5xl font-bold text-center mb-6">
-        Welcome to the quiz
-      </h1>
-      <button className="px-6 py-3 text-lg font-semibold text-white bg-blue-900 rounded-lg hover:bg-blue-950">
-        Start quiz
-      </button>
-    </div>
-  );
+  )
 }
+
+//   return (
+//     <div className="min-h-screen bg-blue-500 flex flex-col justify-center items-center">
+//       <h1 className="text-white text-5xl font-bold text-center mb-6">
+//         Welcome to the quiz
+//       </h1>
+//       <button className="px-6 py-3 text-lg font-semibold text-white bg-blue-900 rounded-lg hover:bg-blue-950">
+//         Start quiz
+//       </button>
+//     </div>
+//   );
+// }
