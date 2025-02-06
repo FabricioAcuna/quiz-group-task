97a223289d7ad5b5c8091b2111a37edd1f857e0b
Jenny Digné <jenny_digne@hotmail.com>
2025-02-06 17:23:27 +0100

    ändrade lite på knapparna för svar

--- pages/quiz.js
+++ pages/quiz.js
@@ -11,7 +11,11 @@ export default function Quiz() {
           return (
             <div key={index}>
               <p>{question.text}</p>
-              <button className="px-6 py-3 text-lg font-semibold text-white bg-blue-900 rounded-lg hover:bg-blue-950">{question.alt}</button>
+              <div className="flex gap-2">
+              <button className="px-3 py-1 text-lg font-semibold text-white bg-blue-900 rounded-lg hover:bg-blue-950">{question.alt[0]}</button>
+              <button className="px-3 py-1 text-lg font-semibold text-white bg-blue-900 rounded-lg hover:bg-blue-950">{question.alt[1]}</button>
+              <button className="px-3 py-1 text-lg font-semibold text-white bg-blue-900 rounded-lg hover:bg-blue-950">{question.alt[2]}</button>
+              </div>
               <p>{question.score}</p>
             </div>
           );
@@ -21,14 +25,4 @@ export default function Quiz() {
   )
 }
 
-//   return (
-//     <div className="min-h-screen bg-blue-500 flex flex-col justify-center items-center">
-//       <h1 className="text-white text-5xl font-bold text-center mb-6">
-//         Welcome to the quiz
-//       </h1>
-//       <button className="px-6 py-3 text-lg font-semibold text-white bg-blue-900 rounded-lg hover:bg-blue-950">
-//         Start quiz
-//       </button>
-//     </div>
-//   );
-// }
+
